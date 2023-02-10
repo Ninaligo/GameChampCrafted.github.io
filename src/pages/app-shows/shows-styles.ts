249507084abf92bbd808bf5e7448e0fbb1920543
Carter Poe <gamechampcrafted@gmail.com>
2023-02-10 02:57:19 -0500

    B

---
+++ src/pages/app-shows/shows-styles.ts
@@ -0,0 +1,7 @@
+import { css } from 'lit';
+
+// these styles can be imported from any component
+// for an example of how to use this, check /pages/about-about.ts
+export const styles = css`
+
+`;
--- src/styles/shared-styles.ts
+++ src/styles/shared-styles.ts
@@ -3,13 +3,24 @@ import { css } from 'lit';
 // these styles can be imported from any component
 // for an example of how to use this, check /pages/about-about.ts
 export const styles = css`
-  @media(min-width: 1000px) {
-    sl-card {
-      max-width: 70vw;
-    }
-  }
+@font-face {
+  font-family: 'Lexend Deca';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url('/src/fonts/fonts/lexend.woff2') format('woff2'), url('/src/fonts/fonts/lexend.woff') format('woff'), url('/fonts/fonts/lexend.ttf') format('truetype')
+}
 
-  main {
-    margin-top: 80px;
-  }
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url('/src/fonts/fonts/roboto.woff2') format('woff2'), url('/src/fonts/fonts/roboto.woff') format('woff'), url('/fonts/fonts/roboto.ttf') format('truetype')
+}
+
+@font-face {
+  font-family: "Glyphicons Halflings";
+  src: url('/src/fonts/fonts/glyph.woff2') format("woff2"), url('/src/fonts/fonts/glyph.woff') format("woff"), url('/src/fonts/fonts/glyph.ttf') format('truetype'), url('/src/fonts/fonts/glyph.svg') format("svg")
+}
 `;
